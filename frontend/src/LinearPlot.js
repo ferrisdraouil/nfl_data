0150aa8523f7cad4fb77c7a10426486be0d438e2
ferrisdraouil <fdraouil@gmail.com>
2019-03-06 16:44:37 -0800

    removed unnecessary component

--- frontend/src/LinearPlot.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ScatterPlot from './ScatterPlot';
-
-export default class LinearGraph extends React.Component {
-  render() {
-    return <ScatterPlot data={this.props.data} title={this.props.title} />;
-  }
-}
